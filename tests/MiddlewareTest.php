b369b7e40070c4ca42409ba9d9b8bc98f46e8cf5
Freek Van der Herten <freek@spatie.be>
2025-02-21 10:46:46 +0100

    fix tests

--- tests/MiddlewareTest.php
+++ tests/MiddlewareTest.php
@@ -10,28 +10,34 @@ function getRoute()
     });
 }
 
-it('adds headers to a response signaling that it may be cached')
-    ->tap(fn () => getRoute()->middleware(CacheWithVarnish::class))
-    ->get('/cache-me')
-    ->assertHeader('X-Cacheable', '1')
-    ->assertHeader('Cache-Control', 'public, s-maxage=86400');
 
-it('uses the config value to determine the name of the header')
-    ->tap(fn () => config()->set('varnish.cacheable_header_name', 'X-My-Custom-Header'))
-    ->tap(fn () => getRoute()->middleware(CacheWithVarnish::class))
-    ->get('/cache-me')
-    ->assertHeader('X-My-Custom-Header', '1')
-    ->assertHeader('Cache-Control', 'public, s-maxage=86400');
+it('adds headers to a response signaling that it may be cached', function () {
+    getRoute()->middleware(CacheWithVarnish::class);
+    $this->get('/cache-me')
+        ->assertHeader('X-Cacheable', '1')
+        ->assertHeader('Cache-Control', 'public, s-maxage=86400');
+});
 
-it('uses the config value to determine the max age')
-    ->tap(fn () => config()->set('varnish.cache_time_in_minutes', 5))
-    ->tap(fn () => getRoute()->middleware(CacheWithVarnish::class))
-    ->get('/cache-me')
-    ->assertHeader('X-Cacheable', '1')
-    ->assertHeader('Cache-Control', 'public, s-maxage=300');
 
-it('accepts an argument to determine the max age')
-    ->tap(fn () => getRoute()->middleware(CacheWithVarnish::class . ':10'))
-    ->get('/cache-me')
-    ->assertHeader('X-Cacheable', '1')
-    ->assertHeader('Cache-Control', 'public, s-maxage=600');
+it('uses the config value to determine the name of the header', function () {
+    config()->set('varnish.cacheable_header_name', 'X-My-Custom-Header');
+    getRoute()->middleware(CacheWithVarnish::class);
+    $this->get('/cache-me')
+        ->assertHeader('X-My-Custom-Header', '1')
+        ->assertHeader('Cache-Control', 'public, s-maxage=86400');
+});
+
+it('uses the config value to determine the max age', function () {
+    config()->set('varnish.cache_time_in_minutes', 5);
+    getRoute()->middleware(CacheWithVarnish::class);
+    $this->get('/cache-me')
+        ->assertHeader('X-Cacheable', '1')
+        ->assertHeader('Cache-Control', 'public, s-maxage=300');
+});
+
+it('accepts an argument to determine the max age', function () {
+    getRoute()->middleware(CacheWithVarnish::class . ':10');
+    $this->get('/cache-me')
+        ->assertHeader('X-Cacheable', '1')
+        ->assertHeader('Cache-Control', 'public, s-maxage=600');
+});
